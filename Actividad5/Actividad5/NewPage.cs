939d85f926bead199de2a6e83ddd5a2ed62994d5
jassonietojuanantonio <jasso_nieto_juan_antonio9@comunidad.unam.mx>
2016-03-28 11:40:07 -0600

    * Actividad5.projitems: actividad 5: creacion de una pantalla de   login
    * NewPage.cs:
    * repositories.config:
    * Actividad5.Android.csproj:
    * Resource.designer.cs:

--- Actividad5/Actividad5/NewPage.cs
+++ Actividad5/Actividad5/NewPage.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Actividad5
+{
+	public class NewPage : ContentPage
+	{
+		public NewPage ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
